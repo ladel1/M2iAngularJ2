c0f096b92ce855d3b3af5ba8d6e504c7008b53d3
LATIBI adel <adel.latibi@gmail.com>
2022-09-28 14:26:27 +0200

    Demo formulaire

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -24,7 +24,8 @@ import { FormGroupComponent } from './components/form-group/form-group.component
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule,// for derctive formGroup
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/form-group/form-group.component.ts
+++ src/app/components/form-group/form-group.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-form-group',
@@ -7,7 +8,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormGroupComponent implements OnInit {
 
-  constructor() { }
+  userForm:FormGroup;
+
+  constructor(
+    private formBuilder:FormBuilder,
+  ) {
+    this.userForm = this.formBuilder.group({
+        nom:['',Validators.required],
+        description:['',]
+    });
+   }
+   checkField(field:string):boolean{
+    return this.userForm.controls[field].invalid && (
+           this.userForm.controls[field].dirty || 
+           this.userForm.controls[field].touched
+      );
+   }
+
+   fieldInvalide():boolean{
+    return this.userForm.controls["nom"].invalid && (
+      this.userForm.controls["nom"].dirty || 
+      this.userForm.controls["nom"].touched
+ );
+   }
+
+   ajouter(){ // ajouter dans api
+    console.log(this.userForm.value)    
+   }
 
   ngOnInit(): void {
   }
